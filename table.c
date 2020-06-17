3e4cc93b9a7ad7a5feae0a6d6f5f5233ab8c3b43
srikanthpragada <srikanthpragada@gmail.com>
2020-06-17 17:57:08 +0530

    17-JUNE

--- table.c
+++ table.c
@@ -1,26 +1,37 @@
-
 // Print table for the given number
+// Usage :  table  <number>
 
 #include <stdio.h>
 
-void main()
+void main(int argc, char * argv[])
 {
-  int num, i;
+  int num, i, len = 10;
 
-     printf("Enter a number :");
-     scanf("%d",&num);
-
-     /*
-     i = 1;
-     while (i <= 10)
+     if(argc < 2)
      {
-       printf("%2d * %2d  = %4d\n",num,i, num * i);
-       i ++;
+        printf("Usage : table <number> [length]\n");
+        printf("Displays table for the given number with the given length.\n");
+        printf("If length is not given then it is taken as 10.\n");
+        return; // stop program
      }
-     */
 
-     for(i=1; i <= 10; i ++)
+     if (argc > 2)
+     {
+         len = atoi(argv[2]);  // Convert third parameter to int
+         if (len == 0)  // if conversion fails
+            len = 10;
+     }
+
+     num = atoi(argv[1]);   // convert string to int
+     if(num == 0)
+     {
+         printf("Sorry! Invalid Number. Please provide a valid number!");
+         return;
+     }
+
+     for(i=1; i <= len; i ++)
      {
         printf("%2d * %2d  = %4d\n",num,i, num * i);
      }
+
 }
